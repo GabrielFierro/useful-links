e86ab477a85b987f262794820584b121a33d5d5f
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-08-15 19:44:22 -0300

    fix: changing the description text and adding the category component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,19 @@
+import Category from '../category/Category';
+
 export default function Header() {
   return (
     <main className='w-full h-auto'>
       <section>
         <article className='w-full text-center mt-12'>
           <h1 className='font-bold text-4xl py-2'>Useful Links</h1>
-          <p className='font-regular text-lg'>
-            A free repository with links for useful websites using Tailwind CSS
+          <p className='font-regular text-lg text-grey'>
+            A free repository with links for useful websites using React and
+            Tailwind CSS
           </p>
         </article>
+      </section>
+      <section className='my-6'>
+        <Category />
       </section>
     </main>
   );
