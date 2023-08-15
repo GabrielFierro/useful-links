ad8d9a8abbc8ae2124086567fc681a4b94c5bff3
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-08-15 19:42:06 -0300

    add: the categories and cards with useState to display the data

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,7 +1,28 @@
+import { useState } from 'react';
+import { data } from './data';
+import Text from './Text';
+import Card from '../card/Card';
+
 export default function Category() {
+  const [card, setCard] = useState(data);
+  const categories = ['All', ...new Set(data.map((card) => card.category))];
+
+  const filter = (cat) => {
+    if (cat === 'All') {
+      setCard(data);
+      return;
+    }
+    setCard(data.filter((item) => item.category === cat));
+  };
+
   return (
     <section>
-      <h1></h1>
+      <article className='w-full h-16 flex flex-row justify-between'>
+        <Text categories={categories} handleClick={filter} />
+      </article>
+      <article className='px-12 mt-16'>
+        <Card allCards={card} />
+      </article>
     </section>
   );
 }
