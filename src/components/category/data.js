97aa057593fc1bcf8341a4b977ad49d5d5103a67
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-08-15 19:42:45 -0300

    add: a starter file with random data

---
+++ src/components/category/data.js
@@ -0,0 +1,43 @@
+import LoopStudios from '../../assets/images/loopstudios.webp';
+import ProductPreview from '../../assets/images/product-preview.webp';
+import Magenta from '../../assets/images/magenta.webp';
+import Sunnyside from '../../assets/images/sunnyside.webp';
+import Travelling from '../../assets/images/travelling.webp';
+
+export const data = [
+  {
+    id: 1,
+    src: LoopStudios,
+    category: 'Design',
+    title: 'Machu Pichu, Peru',
+    rating: 8.6
+  },
+  {
+    id: 2,
+    src: ProductPreview,
+    category: 'CSS',
+    title: 'Venecia, Italy',
+    rating: 7.9
+  },
+  {
+    id: 3,
+    src: Magenta,
+    category: 'React',
+    title: 'Tokyo, Japan',
+    rating: 9
+  },
+  {
+    id: 4,
+    src: Sunnyside,
+    category: 'JavaScript',
+    title: 'Florida, Miami',
+    rating: 8.6
+  },
+  {
+    id: 5,
+    src: Travelling,
+    category: 'HTML',
+    title: 'Alaska, Canada',
+    rating: 8.5
+  }
+];
