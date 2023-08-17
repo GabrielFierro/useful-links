0a1eae7129c71d5b6c95fb674877b129506c51f3
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-08-17 20:17:11 -0300

    fix: changing all the information

--- src/components/category/data.js
+++ src/components/category/data.js
@@ -1,43 +1,143 @@
-import LoopStudios from '../../assets/images/loopstudios.webp';
-import ProductPreview from '../../assets/images/product-preview.webp';
-import Magenta from '../../assets/images/magenta.webp';
-import Sunnyside from '../../assets/images/sunnyside.webp';
-import Travelling from '../../assets/images/travelling.webp';
-
 export const data = [
   {
-    id: 1,
-    src: LoopStudios,
-    category: 'Design',
-    title: 'Machu Pichu, Peru',
-    rating: 8.6
+    category: 'AI',
+    src: 'https://i.ibb.co/DbhbXWR/All-Things-AI.png',
+    title: 'AllThingsAi',
+    description:
+      'Find human-tested reviews of the most useful AI-powered software',
+    url: 'https://allthingsai.com/',
+    date: '18 de Agosto del 2023'
   },
   {
-    id: 2,
-    src: ProductPreview,
-    category: 'CSS',
-    title: 'Venecia, Italy',
-    rating: 7.9
+    category: 'Courses',
+    src: 'https://i.ibb.co/5Lmy6rt/learn-ui-ux-crash-course-2023.jpg',
+    title: 'The UI/UX Crash Course for 2023 - Learn UI/UX Design',
+    description:
+      "You're going to a lot about UI/UX design, along with many of the features and workflows of Figma. From UI design fundamentals such as colors, typography, white space, contrast, color, visual hierarchy & more.",
+    url: 'https://youtu.be/7iobxzd_2wY',
+    date: '18 de Agosto del 2023'
   },
   {
-    id: 3,
-    src: Magenta,
-    category: 'React',
-    title: 'Tokyo, Japan',
-    rating: 9
+    category: 'Courses',
+    src: 'https://lenguajecss.com/large-summary.jpg',
+    title: 'LenguajeCSS',
+    description:
+      'CSS es un lenguaje de estilos para dotar de apariencia y aspecto visual a una página o sitio web (entre otras cosas). También es conocido como el mayor enemigo de los programadores backend. En esta página te ayudo a aprender a dominarlo.',
+    url: 'https://lenguajecss.com/',
+    date: '18 de Agosto del 2023'
   },
   {
-    id: 4,
-    src: Sunnyside,
-    category: 'JavaScript',
-    title: 'Florida, Miami',
-    rating: 8.6
+    category: 'Courses',
+    src: 'https://i.ibb.co/VY3RbZs/cursodereactmidu.jpg',
+    title:
+      'Curso de React [2023]: De cero hasta crear tus primeros componentes con estado',
+    description:
+      'Curso de React desde cero. ¿Qué es? ¿Por qué deberías aprenderlo? ¿Cómo crear tu primera aplicación? Todo lo que necesitas saber para empezar lo tienes en este vídeo.',
+    url: 'https://www.youtube.com/watch?v=7iobxzd_2wY',
+    date: '18 de Agosto del 2023'
   },
   {
-    id: 5,
-    src: Travelling,
+    category: 'Courses',
+    src: 'https://www.aprendejavascript.dev/og.png',
+    title: 'Aprende JavaScript',
+    description:
+      'Curso para aprender paso a paso JavaScript. 100% gratis y práctico. Desde cero.',
+    url: 'https://www.aprendejavascript.dev/',
+    date: '18 de Agosto del 2023'
+  },
+  {
+    category: 'Courses',
+    src: 'https://cdn.freecodecamp.org/platform/universal/fcc_meta_1920X1080-indigo.png',
+    title: 'JavaScript Algorithms and Data Structures',
+    description:
+      "In the JavaScript Algorithm and Data Structures Certification, you'll learn the fundamentals of JavaScript including variables, arrays, objects, loops, and functions.",
+    url: 'https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/',
+    date: '18 de Agosto del 2023'
+  },
+  {
+    category: 'Deploy',
+    src: 'https://assets.vercel.com/image/upload/front/vercel/dps.png',
+    title: 'Vercel',
+    description:
+      "Vercel's frontend cloud gives developers the frameworks, workflows, and infrastructure to build a faster, more personalized Web",
+    url: 'https://vercel.com/',
+    date: '18 de Agosto del 2023'
+  },
+  {
+    category: 'Deploy',
+    src: 'https://www.netlify.com/images/og-image-default.png',
+    title: 'Netlify',
+    description:
+      'Netlify is the modern web development platform for Enterprises to realize the full potential of a scalable, customizable web architecture.',
+    url: 'https://www.netlify.com/',
+    date: '18 de Agosto del 2023'
+  },
+  {
     category: 'HTML',
-    title: 'Alaska, Canada',
-    rating: 8.5
+    src: 'https://cdn.glitch.me/19f7087b-7781-4727-9c59-2100bafabbf2%2Ffront.png?v=1638616243086',
+    title: 'Can Include',
+    description: 'CAN I INCLUDE one TAG to ANOTHER?',
+    url: 'https://caninclude.glitch.me/',
+    date: '18 de Agosto del 2023'
+  },
+  {
+    category: 'Images',
+    src: 'https://www.pexels.com/assets/static/images/meta/pexels-stock-photos.jpg',
+    title: 'Pexels',
+    description:
+      'Las mejores fotos de stock gratis, imágenes libres de regalías y vídeos compartidos por creadores.',
+    url: 'https://www.pexels.com/es-es/',
+    date: '18 de Agosto del 2023'
+  },
+  {
+    category: 'Images',
+    src: 'https://i.ibb.co/KjsPNgC/unsplash.webp',
+    title: 'Unsplash',
+    description:
+      'La fuente de imágenes de internet. Con recursos de creadores de todo el mundo.',
+    url: 'https://unsplash.com/es',
+    date: '18 de Agosto del 2023'
+  },
+  {
+    category: 'Images',
+    src: 'https://i.ibb.co/QJmZ2H1/lorem-picsum.png',
+    title: 'Lorem Picsum',
+    description: 'The Lorem Ipsum for photos.',
+    url: 'https://picsum.photos/',
+    date: '18 de Agosto del 2023'
+  },
+  {
+    category: 'API',
+    src: 'https://i.ibb.co/G3PBRPc/public-apis.png',
+    title: 'Public APIs in GitHub',
+    description:
+      'A collective list of free APIs for use in software and web development.',
+    url: 'https://github.com/public-apis/public-apis',
+    date: '18 de Agosto del 2023'
+  },
+  {
+    category: 'Color',
+    src: 'https://i.ibb.co/kDtx4Cc/coolors.png',
+    title: 'Coolors',
+    description:
+      'Create the perfect palette or get inspired by thousands of beautiful color schemes.',
+    url: 'https://coolors.co/',
+    date: '18 de Agosto del 2023'
+  },
+  {
+    category: 'Color',
+    src: 'https://i.ibb.co/6FJMgVg/colorchecker.png',
+    title: 'Color Contrast Checker by Coolors',
+    description: 'Calculate the contrast ratio of text and background colors.',
+    url: 'https://coolors.co/contrast-checker/000000-c3a9e0',
+    date: '18 de Agosto del 2023'
+  },
+  {
+    category: 'Color',
+    src: 'https://colorhunt.co/img/color-hunt-og.png',
+    title: 'Color Hunt',
+    description: 'Discover the newest hand-picked palettes of Color Hunt.',
+    url: 'https://colorhunt.co/',
+    date: '18 de Agosto del 2023'
   }
 ];
