96e240ec315c89545c8f432a6767156c39219c7f
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-08-17 20:14:28 -0300

    fix: changing the navbar color, and add a new grey color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,13 @@ export default {
   theme: {
     extend: {},
     colors: {
-      navbar: '#E2E4F6',
+      navbar: '#FFF1D0',
       lavender: '#E7C8DD',
       thistle: '#E7C8DD',
       moonstone: '#0FA3B1',
       body: '#F9F7F3',
-      blue: '#B5E2FA'
+      blue: '#B5E2FA',
+      grey: '#767D89'
     }
   },
   plugins: []
